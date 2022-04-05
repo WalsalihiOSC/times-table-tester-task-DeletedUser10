07419a9531cc364da9d0fdd26b15f58619e562c4
Ayush Kumar <90948894+DeletedUser10@users.noreply.github.com>
2022-04-05 20:52:38 +1200

    Next Button command
    -The check label now disappears when the next button is pressed
    - widgets are now white
    - resolution fixed

--- Times tables.py
+++ Times tables.py
@@ -5,24 +5,24 @@ import random
 
 class Times:
     def __init__(self, parent):
-        main_frame = Frame(parent)
+        main_frame = Frame(parent, bg = "White")
         main_frame.grid()
-        title = Label(main_frame, text = "Times table checker", font="Bold")
+        title = Label(main_frame, text = "Times table checker", font="Bold", bg="White")
         title.grid(row=0, column=3, sticky=E)
 
-        self.ql = Label(main_frame, text = "      ", font="Arial")
+        self.ql = Label(main_frame, text = "", font="Arial")
         self.ql.grid(row=1, column=2)
 
         self.en = Entry(main_frame)
         self.en.grid(row=1, column=3)
 
-        check_b = Button(main_frame, text = "Check Answer", command= self.check)
+        check_b = Button(main_frame, text = "Check Answer", command= self.check, bg="White")
         check_b.grid(row=3, column=2)
             
-        self.next_b = Button(main_frame, text = "next", command= self.next)
+        self.next_b = Button(main_frame, text = "next", command= self.next, bg="White")
         self.next_b.grid(row=3, column=3)
 
-        self.checkl = Label(main_frame, text = "")
+        self.checkl = Label(main_frame, text = "", bg = "White")
         self.checkl.grid(row=1, column=4)
 
         self.next()
@@ -32,7 +32,8 @@ class Times:
         numb2 = random.randint(1,10)
         self.multiply = str(numb1*numb2)
 
-        self.ql.configure(text = f"{numb1} * {numb2} = ")
+        self.ql.configure(text = f"{numb1} x {numb2} = ", bg = "White")
+        self.checkl.configure(text="", font="Arial")
    
     def check(self):
         if self.en.get() != self.multiply:
@@ -47,7 +48,8 @@ class Times:
 def main():
     root = Tk()
     Times(root)
-    root.geometry("300x100")
+    root.geometry("350x100")
+    root.configure(bg="white")
 
     root.mainloop()
 
